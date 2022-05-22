af78c712a3dc31b285ce8209bdb62c1f370859a6
Juan David Mercado Lopez <juandmercadolopez@gmail.com>
2022-05-22 15:26:23 -0500

    creando servicio eureka

---
+++ README.md
@@ -0,0 +1,1 @@
+# juandmercadolopez-meli-service-registry
---
+++ src/test/java/com/juandmercadolopez/meli/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juandmercadolopez.meli.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
